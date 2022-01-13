6445fcb02bdd9b9e6aa34dd4f9c008f332acc358
Valdenir Roman Junior <vromanjunior@outlook.com>
2022-01-13 16:02:00 -0300

    Save Score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmovie.entities.Score;
+import com.devsuperior.dsmovie.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>  {
+	
+	User findByEmail(String email);
+
+	Score saveAndFlush(Score score);
+	
+
+}
